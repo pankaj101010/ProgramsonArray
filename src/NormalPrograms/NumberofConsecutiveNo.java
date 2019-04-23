aba92447bfbea3bb46817d1a0617ff363bde68ca
bharadiya <shashank.bharadiya@gmail.com>
2019-04-23 17:26:57 +0530

    Added Number of Consecutive Numbers

--- src/NormalPrograms/NumberofConsecutiveNo.java
+++ src/NormalPrograms/NumberofConsecutiveNo.java
@@ -2,22 +2,28 @@ package NormalPrograms;
 
 public class NumberofConsecutiveNo {
 	public static int isSumOfConsecutive2(int n) {
-
-		for (int i = 0; i < n; i++) {
-
-		}
-		return n;
-	}
-
-	public static int doingCalculation(int i, int n) {
 		int sum = 0;
-		for (int j = i; j < n; j++) {
-			sum += j + (j + 1);
+		int count = 0;
+		for (int pair = 1; pair < (n / 2) - 1; pair++) {
+			for (int i = 1; i <= n / 2; i++) {
+				sum += (pair + 1) * i + ((pair + 1) * pair) / 2;
+				if (sum == n) {
+					count++;
+				}
+				sum = 0;
+			}
 		}
-		return sum;
+		return count;
+//		int s = 0;
+//		int i = 1;
+//		while (++i < n)
+//			if (n % ++i < 1)
+//				s++;
+//		return s;
 	}
 
 	public static void main(String[] args) {
-
+		int n = 8;
+		System.out.println(isSumOfConsecutive2(n));
 	}
 }
