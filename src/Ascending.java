25f303930b7b79567fa00a033446e468baaa17cb
pankaj1010 <pankajdevgade10@gmail.com>
2019-04-27 23:01:48 +0530

    check for conflicts

--- src/Ascending.java
+++ src/Ascending.java
@@ -1,16 +1,16 @@
 
 public class Ascending {
 	public static void main(String[] args) 
-	// comment added
+	// comment added checked
 	{
 		int[] arr = { 10, 9, 8, 6, 3, 11, 11, 11, 1, 1, 9, 8, 7 };
-		int temp = 0;
+		int temp1 = 0;
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = 0; j < arr.length - i - 1; j++) {
 				if (arr[j] < arr[j + 1]) {
-					temp = arr[j];
+					temp1 = arr[j];
 					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
+					arr[j + 1] = temp1;
 				}
 			}
 		}
